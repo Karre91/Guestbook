4c3b08ba7ad17161299749f4982c174ccd70ec2e
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2021-12-01 17:06:05 +0000

    Beginning to get a real form with writing out the previous posts made, a couple attempts at JSON but none working so far.

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-const express = require("express"); // "require" betyder att man importerar en modul
+const express = require("express");
+const fs = require("fs"); 
 const app = express();
-app.get("/", function(req, res) {   // definiera en route som lyssnar på get-förfrågningar
-    res.send("<h1>Hallå Express!</h1>");    // med metoden "send" istället för "end" så visas svenska bokstäver korrekt
-    // Express har en funktion "send" som även skickar headerinformation som t.ex. teckenkodning
-});
-app.listen(3000);   // STARTAR SERVERN OCH LYSSNAR PÅ PORT 3000
-console.log("Kör servern på localhost:3000");
+const port = 3000;
+const bodyParser = require("body-parser");
 
-app.get("/undersida", function(req, res) {
-    res.send("Välkommen till undersidan!");
+app.use(express.static("public"));
+app.use(express.urlencoded());
+
+app.get("/form", (req, res) => {  
+    res.sendFile(__dirname + "/index.html");  
 });
 
-const fs = require("fs");   // eventuellt behöver ni köra "npm install fs"
-app.get(" ", function(req, res) {
-    fs.readFile("Hej.txt", function(err, data) {   // första argumentet är filnamnet, andra argumentet är en funktion (callback)
-        // callback-funktionen har i sin tur två argument: ett objekt err med information om eventuella fel och ett objekt med data som lästs in
-        if (err) throw err; // avbryt exekveringen vid fel
-        else console.log(err);  // null om inget fel inträffat
-        res.send(data);
-    });
+app.post("/form", (req,res) => {
+    
+    let name = req.body.name; 
+    let email = req.body.email;
+    let tel = req.body.tel;
+    let comment = req.body.comment;
+
+    let all = `Namn: ${name}<br>Email: ${email}<br>Tel: ${tel}<br>Kommentar: ${comment}<br><br>\n`;
+    
+       
+    // fs.appendFile("guestbook.txt", all, (err) => {
+    //     if (err) throw err; 
+    //     const content = fs.readFileSync("guestbook.txt");
+    //     fs.readFile("guestbook.txt", (err) => {
+    //         if (err) throw err;                
+    //         res.send(content.toString());         
+    //     });      
+    // });     
+});
+
+app.listen(port, () => {
+    console.log("Kör servern på localhost:3000");
 });
